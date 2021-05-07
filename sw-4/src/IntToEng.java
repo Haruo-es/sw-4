ae99bdce53d7dab2e33999f557478102c81a1040
Minami Shibuya <g18946sm@gm.tsuda.ac.jp>
2021-05-07 17:06:06 +0900

    テストできんTT

--- sw-4/src/IntToEng.java
+++ sw-4/src/IntToEng.java
@@ -9,8 +9,9 @@ public class IntToEng {
  
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
-    	String[] num1= {"zero","one","two","three","four","five","six","seven","eight","nine","ten"};
-    	if(n=0||n<11)return num1[n];
-        return "";
+    	String[] num1= {"zero","one","two","three","four","five","six","seven","eight","nine","ten",
+    			"eleven","twelve","thirteen","fourteen","fifteen","sixteen","eighteen","nineteen"};
+    	//if(n=0||n<11)
+    	return num1[n];
     }
 }

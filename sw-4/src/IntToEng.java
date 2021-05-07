e240199ed274669a9ff22613913120931f606466
usudarisa <usudarisa@localhost>
2021-05-07 16:23:58 +0900

    エラー祭り

--- sw-4/src/IntToEng.java
+++ sw-4/src/IntToEng.java
@@ -6,9 +6,11 @@ public class IntToEng {
         int input = sc.nextInt();
         System.out.println(translateEng(input));
     }
-
+ 
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
+    	String[] num1= {"zero","one","two","three","four","five","six","seven","eight","nine","ten"};
+    	if(n=0||n<11)return num1[n];
         return "";
     }
 }
